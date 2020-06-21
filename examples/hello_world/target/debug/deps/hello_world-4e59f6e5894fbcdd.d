c343950f781bbf09c43c9036f17a898731ab3012
许丹琦 <1204253728@qq.com>
2020-06-21 22:05:37 +0800

    cargo new hello_world

---
+++ examples/hello_world/target/debug/deps/hello_world-4e59f6e5894fbcdd.d
@@ -0,0 +1,5 @@
+/Users/xudanqi/Desktop/setsail/Goo-rust/Goo-jsToRust/examples/hello_world/target/debug/deps/hello_world-4e59f6e5894fbcdd: src/main.rs
+
+/Users/xudanqi/Desktop/setsail/Goo-rust/Goo-jsToRust/examples/hello_world/target/debug/deps/hello_world-4e59f6e5894fbcdd.d: src/main.rs
+
+src/main.rs:
---
+++ examples/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/xudanqi/Desktop/setsail/Goo-rust/Goo-jsToRust/examples/hello_world/target/debug/hello_world: /Users/xudanqi/Desktop/setsail/Goo-rust/Goo-jsToRust/examples/hello_world/src/main.rs
